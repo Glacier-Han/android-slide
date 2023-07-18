25d8cbd67f6243a9be8640a003e61a98cb433585
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2023-07-18 16:58:56 +0900

    feat: SquareSlide 객체 생성 및 팩토리 생성, 테스트 로그 띄우기

--- app/src/main/java/com/glacier/androidslide/MainActivity.kt
+++ app/src/main/java/com/glacier/androidslide/MainActivity.kt
@@ -2,10 +2,24 @@ package com.glacier.androidslide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.glacier.androidslidesofteer.model.SquareSlide
+import com.glacier.androidslidesofteer.model.SquareSlideViewFactory
+import com.glacier.androidslidesofteer.util.UtilManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val squareSlides = mutableListOf<SquareSlide>()
+        squareSlides.add(SquareSlideViewFactory.createItem(UtilManager.generateID(9)))
+        squareSlides.add(SquareSlideViewFactory.createItem(UtilManager.generateID(9)))
+        squareSlides.add(SquareSlideViewFactory.createItem(UtilManager.generateID(9)))
+        squareSlides.add(SquareSlideViewFactory.createItem(UtilManager.generateID(9)))
+
+        for (i in 0..3) {
+            Log.d("DBG::SLIDE", "Rect${i + 1} ${squareSlides[i]}")
+        }
     }
 }
