d8c4368a7004e597d6b4459ea80443d835d71caa
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2023-07-20 11:35:14 +0900

    feat: 불러온 사진 데이터는 바이너리 형태로 메모리에서 관리하는 요구사항 적용 위해 image 자료형을 ByteArray로 변경

--- app/src/main/java/com/glacier/androidslide/model/ImageSlide.kt
+++ app/src/main/java/com/glacier/androidslide/model/ImageSlide.kt
@@ -7,10 +7,25 @@ data class ImageSlide(
     override var side: Int,
     override var alpha: Int,
     override var selected: Boolean,
-    var image: String,
+    var image: ByteArray,
 ) : Slide {
     override val slideType: SlideType = SlideType.IMAGE
     override fun toString(): String {
         return "$slideType - ($id), Side:$side, Image:$image, Alpha:$alpha, Selected:$selected"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ImageSlide
+
+        if (!image.contentEquals(other.image)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return image.contentHashCode()
     }
 }
