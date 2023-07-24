c2e4342322138a94ad72a81ebe62ff5e354453f7
Glacier Han <61905052+Glacier-Han@users.noreply.github.com>
2023-07-24 10:41:18 +0900

    feat: Retrofit2 라이브러리 추가 및 서버의 변수명을 인식하기 위한 SerializedName 어노테이션 추가

--- app/src/main/java/com/glacier/androidslide/model/SlideColor.kt
+++ app/src/main/java/com/glacier/androidslide/model/SlideColor.kt
@@ -1,7 +1,9 @@
 package com.glacier.androidslide.model
 
+import com.google.gson.annotations.SerializedName
+
 data class SlideColor(
-    val r: Int,
-    val g: Int,
-    val b: Int
+    @SerializedName("R") val r: Int,
+    @SerializedName("G") val g: Int,
+    @SerializedName("B") val b: Int
 )
